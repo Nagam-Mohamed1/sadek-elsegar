a7451ad207cc328ff27b5f7d3280e23d50f2a3cc
Nagam-Mohamed1 <132608112+Nagam-Mohamed1@users.noreply.github.com>
2023-05-04 22:25:44 +0300

    Add files via upload

--- components/footer.php
+++ components/footer.php
@@ -2,25 +2,25 @@
   <div class="row">
     <div class=" col-12 col-md-4">
       <h1 style="color:black;text-align:right">الأنشطة</h1>
-      <a href="../Quraan/index.php#programs">برامج</a>
-      <a href="../Quraan/index.php#workshop">ورش</a>
-      <a href="../Quraan/index.php#ask">استشارات</a>
+      <a href="../index.php#programs">برامج</a>
+      <a href="../index.php#workshop">ورش</a>
+      <a href="../index.php#ask">استشارات</a>
     </div>
     <div class=" col-12 col-md-4">
       <h1 style="color:black;text-align:right">ورش وبرامج حديثة</h1>
-      <a href="../Quraan/esraa.php">سبحان الذي أسرى</a>
-      <a href="../Quraan/chemist.php">الكيميائي </a>
-      <a href="../Quraan/animals.php">فاقصص القصص </a>
-      <a href="../Quraan/colors.php">ورشة الألوان </a>
-      <a href="../Quraan/moon.php">ورشة والقمر إذا اتسق</a>
-      <a href="../Quraan/bones.php">ورشة العظام</a>
+      <a href="../esraa.php">سبحان الذي أسرى</a>
+      <a href="../chemist.php">الكيميائي </a>
+      <a href="../animals.php">فاقصص القصص </a>
+      <a href="../colors.php">ورشة الألوان </a>
+      <a href="../moon.php">ورشة والقمر إذا اتسق</a>
+      <a href="../bones.php">ورشة العظام</a>
     </div>
     <div class="aboutusi col-12 col-md-4">
       <h1 style="color:black;text-align:right">
     عن المنصة
   </h1>  
-      <a href="../Quraan/index.php#home">الرئيسية</a>
-      <a href="../Quraan/index.php#about">من نحن</a>
+      <a href="../index.php#home">الرئيسية</a>
+      <a href="../index.php#about">من نحن</a>
       <a href="#cont">تواصل معنا</a>
 
     </div>
